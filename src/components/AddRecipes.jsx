9bae6aa72b134d08fbbb559b331bc707c94e9f73
simonmwaura <simonmwaura2090@gmail.com>
2024-05-07 21:44:52 +0300

    assignment completed

--- src/components/AddRecipes.jsx
+++ src/components/AddRecipes.jsx
@@ -1,43 +1,65 @@
 import React, { useEffect, useState }  from 'react'
-import Post from '../recipe component/Post';
+
 
 export default function AddRecipes() 
 {
-const[posts,setPosts]=useState([])
+const[recipename,setRecipeName]=useState()
+const[ingredients,setIngredients]=useState()
+const[listofingredients,setListofingredients]=useState()
+const[cookinginstruction,setCookinginstructions]=useState()
+const[listofcookinginstruction,setListofcookinginstructions]=useState()
+const[images,setImages]=useState()
+const[comments,setComments]=useState([])
 
-useEffect(()=>{
 
-  fetch('http://localhost:3000/posts')
-  .then((response) => response.json())
-  .then((data) =>{
-    setPosts(data)
-  } );
-},[])
+console.log(recipename)
 
-// console.log(posts[0].comments[0].text);
-  
+
+function handleSubmit(e){
+     e.preventDefault()
+
+     console.log(recipename,ingredients,listofingredients,listofcookinginstruction,cookinginstruction,images)
+}
+
   return (
-    <div>
+    <div className='flex  bg-green-900'>
+          <div className='w-1/2 p-16 text-white '>
+            <img src="./images/uzi.png" alt="image" className='w-full h-full object-cover rounded-lg bg-green-800'/>
+          </div>
+  <div class='w-1/2 p-4'>
+    <h1 className='text-5xl  text-white text-center font-bold pb-16'>Please add your recipe here</h1>
+    {/* <!-- Add the rest of your text content here --> */}
 
-<div className=' mx-auto p-4 flex flex-wrap bg-green-900'>
-        <div className='w-full md:w-1/2 xl:w-1/2 p-4'>
-          <img src="./images/uzi.png" alt="image" className='w-full h-full object-cover rounded-lg bg-green-800'/>
-        </div>
-        <div className='w-full md:w-1/2 xl:w-1/2 p-4 '>
-        
-
-        </div>
-       
-
-
-
-    </div>
-    {
-        posts.map((post)=>(
-          <Post post={post}/>
-        ))
-       }
-    </div>
+    <form class="max-w-md mx-auto " onSubmit={handleSubmit}>
+      
+  <div class="relative z-0 w-full mb-5 group">
+      <input value={recipename} onChange={(e)=>setRecipeName(e.target.value)} type="text" className="justify-center items-center block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
+      <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">kindly enter the name of your recipe </label>
+  </div>
+  <div class="relative z-0 w-full mb-5 group">
+      <input value={ingredients} onChange={(e)=>setIngredients(e.target.value)} type="text" className="justify-center items-center block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
+      <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">kindly enter the Word ingredients </label>
+  </div>
+  <div class="relative z-0 w-full mb-5 group">
+      <textarea value={listofingredients} onChange={(e)=>setListofingredients(e.target.value)} type="text" className="justify-center items-center block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required>  </textarea>  
+      <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">kindly enter the list of ingredients </label>
+  </div>
+  <div class="relative z-0 w-full mb-5 group">
+      <input value={cookinginstruction} onChange={(e)=>setCookinginstructions(e.target.value)} type="text" className="justify-center items-center block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
+      <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">kindly enter the words cooking instructions </label>
+  </div>
+  <div class="relative z-0 w-full mb-5 group">
+      <textarea value={listofcookinginstruction} onChange={(e)=>setListofcookinginstructions(e.target.value)} type="text" className="justify-center items-center block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required>  </textarea>  
+      <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">kindly enter the set of cooking instructions </label>
+  </div>
+  <div class="relative z-0 w-full mb-5 group">
+      <textarea value={images} onChange={(e)=>setImages(e.target.value)} type="text" className="justify-center items-center block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required></textarea>
+      <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">kindly add the image of the recipe </label>
+  </div>
+  <button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Save post</button>
+  </form>
+  </div>
+   </div>
     
 
   
