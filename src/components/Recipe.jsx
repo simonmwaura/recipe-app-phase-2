25e421e7d079d77599e6edee04b0d22b8b0b8474
simonmwaura <simonmwaura2090@gmail.com>
2024-05-08 10:55:27 +0300

    assignment

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -14,6 +14,33 @@ export default function Recipe() {
 },[])
 
  
+
+ // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = posts.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(posts.length / postsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * postsPerPage) % posts.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+
+
+
+
+  
   return (
     
     <div>
@@ -21,7 +48,7 @@ export default function Recipe() {
       <div className='grid md:grid-cols-3  grid-cols-1 gap-6'>
 
       {
-        posts.map((post)=>(
+       currentItems && currentItems .map((post)=>(
           <Post  key={post.id} post={post}/>
         ))
        }
--- src/recipe component/Post.jsx
+++ src/recipe component/Post.jsx
@@ -10,7 +10,10 @@ export default function Post({post}) {
   return (
     <div>
 
-<Link to={`/blog/${post.id}`} className='border p-5' key={post.id}>
+<Link to={`/blog/${post.id}`} className='border p-5 bg-green-600' key={post.id}>
+<div>
+        <img class="rounded-t-lg h-[50vh] w-full" src="https://images.pexels.com/photos/2762942/pexels-photo-2762942.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+    </div>
             <h1 className='text-xl font-semibold'>{post.recipename}</h1>
             <h4 className='font-extrabold text-2xl '>ingredients</h4>
             <p>{post.listofingredients}</p>
