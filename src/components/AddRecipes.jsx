b10a0ca45a96001a142734d8e93b2f8c3fef8eb8
simonmwaura <simonmwaura2090@gmail.com>
2024-05-09 23:59:51 +0300

    assignment completed

--- src/components/AddRecipes.jsx
+++ src/components/AddRecipes.jsx
@@ -37,7 +37,7 @@ fetch('https://recipe-app-phase-2-1.onrender.com/posts', {
   return (
     <div className='flex flex-col md:flex-row bg-white'>
         <div className='md:w-1/2 p-16 md:p-0 text-center md:text-left text-white'>
-            <img src="./images/uzi.png" alt="image" className='w-full h-full object rounded-lg bg-white '/>
+            <img src="https://www.stockvault.net/data/2017/01/29/221904/preview16.jpg" alt="image" className='w-full h-full object rounded-lg bg-white '/>
         </div>
 
        <div className='w-full md:w-1/2 p-4'>
